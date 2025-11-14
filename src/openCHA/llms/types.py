ff2350f2652fea053661c479d9f45a1f1a416f61
laianapinho <laiana.cavalcante15@gmail.com>
2025-11-14 10:27:04 -0400

    Adicionando suporte para o DeepSeek

--- src/openCHA/llms/types.py
+++ src/openCHA/llms/types.py
@@ -6,9 +6,11 @@ from openCHA.llms import BaseLLM
 from openCHA.llms import LLMType
 from openCHA.llms import OpenAILLM
 from openCHA.llms import GeminiLLM
+from openCHA.llms.deepseek import DeepSeekLLM
 
 LLM_TO_CLASS: Dict[LLMType, Type[BaseLLM]] = {
     LLMType.OPENAI: OpenAILLM,
     LLMType.ANTHROPIC: AntropicLLM,
+    LLMType.DEEPSEEK: DeepSeekLLM,
     LLMType.GEMINI: GeminiLLM
 }
