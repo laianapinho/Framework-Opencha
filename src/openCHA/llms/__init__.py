c1a6d72d6152222d720c276c9c1c22b64efa8424
laianapinho <laiana.cavalcante15@gmail.com>
2025-11-14 10:25:41 -0400

    Adicionando suporte para o DeepSeek

--- src/openCHA/llms/__init__.py
+++ src/openCHA/llms/__init__.py
@@ -5,12 +5,14 @@ from openCHA.llms.openai import OpenAILLM
 from openCHA.llms.gemini import GeminiLLM
 from openCHA.llms.types import LLM_TO_CLASS
 from openCHA.llms.initialize_llm import initialize_llm
+from openCHA.llms.deepseek import DeepSeekLLM
 
 __all__ = [
     "BaseLLM",
     "AntropicLLM",
     "OpenAILLM",
     "GeminiLLM",
+    "DeepSeekLLM",
     "LLMType",
     "LLM_TO_CLASS",
     "initialize_llm",
