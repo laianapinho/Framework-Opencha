7cea50ae46bc1b7951c0b137d2477cdf349822be
laianapinho <laiana.cavalcante15@gmail.com>
2025-11-19 17:14:29 -0400

    Removendo módulos de tasks afetadas e atualizando tipos

--- src/openCHA/tasks/task_types.py
+++ src/openCHA/tasks/task_types.py
@@ -1,23 +1,10 @@
 from enum import Enum
 
 class TaskType(str, Enum):
-    AWAKENINGS_DETECTION = "awakenings_detection"
-    SLEEP_DISORDERS_INTERVALS = "sleep_disorders_intervals"
-    EDA_FEATURE_EXTRACTION = "eda_feature_extraction"
-    SLEEP_QUALITY_FROM_EDA = "sleep_quality_from_eda"
     SERPAPI = "serpapi"
     EXTRACT_TEXT = "extract_text"
-    AFFECT_SLEEP_GET = "affect_sleep_get"
-    AFFECT_ACTIVITY_GET = "affect_activity_get"
-    AFFECT_SLEEP_ANALYSIS = "affect_sleep_analysis"
-    AFFECT_ACTIVITY_ANALYSIS = "affect_activity_analysis"
     GOOGLE_TRANSLATE = "google_translate"
     ASK_USER = "ask_user"
     TEST_FILE = "test_file"
     RUN_PYTHON_CODE = "run_python_code"
-    PPG_GET = "affect_ppg_get"
-    PPG_ANALYSIS = "affect_ppg_analysis"
-    STRESS_ANALYSIS = "affect_stress_analysis"
-    QUERY_NUTRITIONIX = "query_nutritionix"
-    CALCULATE_FOOD_RISK_FACTOR = "calculate_food_risk_factor"
     GOOGLE_SEARCH = "google_search"
--- src/openCHA/tasks/types.py
+++ src/openCHA/tasks/types.py
@@ -10,39 +10,13 @@ from openCHA.tasks import RunPythonCode
 from openCHA.tasks import SerpAPI
 from openCHA.tasks import TaskType
 from openCHA.tasks import TestFile
-from openCHA.tasks.affect import ActivityAnalysis
-from openCHA.tasks.affect import ActivityGet
-from openCHA.tasks.affect import PPGAnalysis
-from openCHA.tasks.affect import PPGGet
-from openCHA.tasks.affect import SleepAnalysis
-from openCHA.tasks.affect import SleepGet
-from openCHA.tasks.affect import StressAnalysis
-from openCHA.tasks.custom import AwakeningsDetection, SleepDisordersIntervals, EDAFeatureExtraction, SleepQualityFromEDA
-from openCHA.tasks.nutritionix import (
-    CalculateFoodRiskFactor,
-)
-from openCHA.tasks.nutritionix import QueryNutritionix
-
 
 TASK_TO_CLASS: Dict[TaskType, Type[BaseTask]] = {
     TaskType.SERPAPI: SerpAPI,
     TaskType.EXTRACT_TEXT: ExtractText,
-    TaskType.AFFECT_SLEEP_GET: SleepGet,
-    TaskType.AFFECT_ACTIVITY_GET: ActivityGet,
-    TaskType.AFFECT_SLEEP_ANALYSIS: SleepAnalysis,
-    TaskType.AFFECT_ACTIVITY_ANALYSIS: ActivityAnalysis,
     TaskType.GOOGLE_TRANSLATE: GoogleTranslate,
     TaskType.ASK_USER: AskUser,
     TaskType.TEST_FILE: TestFile,
     TaskType.RUN_PYTHON_CODE: RunPythonCode,
-    TaskType.PPG_GET: PPGGet,
-    TaskType.PPG_ANALYSIS: PPGAnalysis,
-    TaskType.STRESS_ANALYSIS: StressAnalysis,
-    TaskType.QUERY_NUTRITIONIX: QueryNutritionix,
-    TaskType.CALCULATE_FOOD_RISK_FACTOR: CalculateFoodRiskFactor,
     TaskType.GOOGLE_SEARCH: GoogleSearch,
-    TaskType.AWAKENINGS_DETECTION: AwakeningsDetection,
-    TaskType.SLEEP_DISORDERS_INTERVALS: SleepDisordersIntervals,
-    TaskType.EDA_FEATURE_EXTRACTION: EDAFeatureExtraction,
-    TaskType.SLEEP_QUALITY_FROM_EDA: SleepQualityFromEDA,
 }
